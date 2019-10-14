d4b615ce4e53905d81d5c067903a90fe12b0683b
Artlec <38135800+arthlec@users.noreply.github.com>
2019-10-14 15:45:51 +0200

    Indicator Pane second Version with menuButton

--- src/com.esiee.hmi.view/src/com/esiee/hmi/view/GlobalPane.java
+++ src/com.esiee.hmi.view/src/com/esiee/hmi/view/GlobalPane.java
@@ -2,18 +2,29 @@ package com.esiee.hmi.view;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 public class GlobalPane extends SplitPane {
 
     public GlobalPane(){
-        StackPane stackPane = new StackPane();
+        AnchorPane anchorPane = new AnchorPane();
         Button menuButton = new Button("Menu");
-        menuButton.setTranslateX(20);
-        stackPane.getChildren().addAll(new MapPane(), menuButton);
+        MapPane map = new MapPane();
 
-        this.getItems().addAll(new IndicatorPane(), stackPane);
-//        this.setDividerPositions(0f);
+        AnchorPane.setLeftAnchor(menuButton, 20d);
+
+        this.heightProperty().addListener((observable, oldValue, newValue) -> AnchorPane.setTopAnchor(menuButton, newValue.doubleValue()/2));
+
+        AnchorPane.setLeftAnchor(map, 0d);
+        AnchorPane.setRightAnchor(map, 0d);
+        AnchorPane.setTopAnchor(map, 0d);
+        AnchorPane.setBottomAnchor(map, 0d);
+
+        anchorPane.getChildren().addAll(map, menuButton);
+
+        this.getItems().addAll(new IndicatorPane(), anchorPane);
     }
 
 }
