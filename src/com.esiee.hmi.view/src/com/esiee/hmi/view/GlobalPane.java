6e93d21a8f97ddb76a8be7807805689361e9a2f2
Artlec <38135800+arthlec@users.noreply.github.com>
2019-10-21 17:03:32 +0200

    paint categories 2nd version

--- src/com.esiee.hmi.view/src/com/esiee/hmi/view/GlobalPane.java
+++ src/com.esiee.hmi.view/src/com/esiee/hmi/view/GlobalPane.java
@@ -4,16 +4,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.Map;
+
 public class GlobalPane extends SplitPane {
 
     private boolean isMenuClosed;
     private IndicatorPane menu;
+    private MapPane mapPane;
 
     public GlobalPane(){
         this.isMenuClosed = false;
-        this.menu = new IndicatorPane();
+        this.menu = new IndicatorPane(this);
         AnchorPane anchorPane = new AnchorPane();
-        MapPane map = new MapPane();
+        this.mapPane = new MapPane();
 
         Button menuButton = new Button("Menu");
         menuButton.setOnMouseClicked(event -> closeOrOpenMenu());
@@ -22,12 +25,12 @@ public class GlobalPane extends SplitPane {
 
         this.heightProperty().addListener((observable, oldValue, newValue) -> AnchorPane.setTopAnchor(menuButton, newValue.doubleValue()/2));
 
-        AnchorPane.setLeftAnchor(map, 0d);
-        AnchorPane.setRightAnchor(map, 0d);
-        AnchorPane.setTopAnchor(map, 0d);
-        AnchorPane.setBottomAnchor(map, 0d);
+        AnchorPane.setLeftAnchor(this.mapPane, 0d);
+        AnchorPane.setRightAnchor(this.mapPane, 0d);
+        AnchorPane.setTopAnchor(this.mapPane, 0d);
+        AnchorPane.setBottomAnchor(this.mapPane, 0d);
 
-        anchorPane.getChildren().addAll(map, menuButton);
+        anchorPane.getChildren().addAll(this.mapPane, menuButton);
 
         this.getItems().addAll(this.menu, anchorPane);
     }
@@ -40,6 +43,10 @@ public class GlobalPane extends SplitPane {
             this.setDividerPositions(this.menu.getMinWidth());
             this.isMenuClosed = true;
         }
+    }
+
+    public MapPane getMapPane(){
+        return this.mapPane;
     }
 
 }
--- src/com.esiee.hmi.view/src/com/esiee/hmi/view/IndicatorPane.java
+++ src/com.esiee.hmi.view/src/com/esiee/hmi/view/IndicatorPane.java
@@ -4,12 +4,13 @@ import com.esiee.hmi.dataaccess.WDIIndicatorDataDecoder;
 import com.esiee.hmi.model.DataManager;
 import com.esiee.hmi.model.countries.Country;
 import com.esiee.hmi.model.countries.GeoPoint;
-import com.esiee.hmi.model.countries.Polygon;
 import com.esiee.hmi.model.indicators.Indicator;
 import com.esiee.hmi.model.indicators.IndicatorData;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 
 import java.io.IOException;
 import java.util.*;
@@ -20,11 +21,13 @@ import java.util.stream.Stream;
 public class IndicatorPane extends Accordion {
 
     private HashMap<String, Color> listOfColors;
+    private GlobalPane parent;
 
-    public IndicatorPane(){
+    public IndicatorPane(GlobalPane parent){
         this.setMaxWidth(550);
         this.setPrefWidth(550);
         this.setMinWidth(0);
+        this.parent = parent;
         Map<String, Map<String, List<Indicator>>> indicatorsMap = DataManager.INSTANCE.getIndicatorsMap();
         List<TitledPane> listOfPanes = new ArrayList<>();
         this.listOfColors = new HashMap<>();
@@ -81,14 +84,25 @@ public class IndicatorPane extends Accordion {
         if (optionalIndicator.isPresent()) {
             try {
                 indicatorDataList = WDIIndicatorDataDecoder.decode(optionalIndicator.get().getCode());
-                System.out.println(indicatorDataList);
+//                System.out.println(indicatorDataList);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+            if(indicatorDataList == null)
+                return;
+
             for(IndicatorData indicatorData : indicatorDataList) {
                 double max = 0.0;
                 double min = Double.MAX_VALUE;
+                System.out.println(indicatorData.getCountryCode());
+                Optional<Country> optionalCountry = DataManager.INSTANCE.getCountryByCode(indicatorData.getCountryCode());
+                Country c = null;
+                if (optionalCountry.isPresent())
+                    c = optionalCountry.get();
+                else
+                    return;
+
                 for (double data : indicatorData.getValues()){
                     if(!Double.isNaN(data)){
 //                    System.out.println(data);
@@ -110,31 +124,24 @@ public class IndicatorPane extends Accordion {
 
                 for (double data : indicatorData.getValues())
                     if(!Double.isNaN(data)){
-//                    System.out.println(data);
-                        for (Country c : DataManager.INSTANCE.getCountries()) {
-                            List<Polygon> poly = c.getGeometry().getPolygons();
-                            for (Polygon polygon : poly) {
-                                javafx.scene.shape.Polygon tamponPolygon = new javafx.scene.shape.Polygon();
-                                for (GeoPoint geos : polygon.points) {
-                                    tamponPolygon.getPoints().addAll(geos.lon, geos.lat);
-                                }
-//                            System.out.println("OK");
+                        MapPane mapPane = this.parent.getMapPane();
+//                        System.out.println(mapPane);
+                        for(Node node : mapPane.getChildren()){
+                                javafx.scene.shape.Polygon polygon = (javafx.scene.shape.Polygon) node;
                                 if(data < deuxiemecatLimit)
-//                                System.out.println("LIME");
-                                    tamponPolygon.setFill(listOfColors.get("LIME"));
+                                    polygon.setFill(listOfColors.get("LIME"));
                                 else if(data < troisiemecatLimit)
-                                    tamponPolygon.setFill(listOfColors.get("LIMEGREEN"));
+                                    polygon.setFill(listOfColors.get("LIMEGREEN"));
                                 else if(data < quatriemecatLimit)
-                                    tamponPolygon.setFill(listOfColors.get("DARKGREEN"));
+                                    polygon.setFill(listOfColors.get("DARKGREEN"));
                                 else if(data < cinquiemecatLimit)
-                                    tamponPolygon.setFill(listOfColors.get("NAVAJOWHITE"));
+                                    polygon.setFill(listOfColors.get("NAVAJOWHITE"));
                                 else if(data < sixiemecatLimit)
-                                    tamponPolygon.setFill(listOfColors.get("TOMATO"));
+                                    polygon.setFill(listOfColors.get("TOMATO"));
                                 else if(data < septiemecatLimit)
-                                    tamponPolygon.setFill(listOfColors.get("ORANGE"));
+                                    polygon.setFill(listOfColors.get("ORANGE"));
                                 else
-                                    tamponPolygon.setFill(listOfColors.get("RED"));
-                            }
+                                    polygon.setFill(listOfColors.get("RED"));
                         }
                     }
             }
